0d62d8485308e42b2da74cf47db5c46a8101266e
haposan06 <johaposan@gmail.com>
2016-05-22 21:42:29 +0700

    2nd

---
+++ scripttest.js
@@ -0,0 +1,5 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var prop in names) {
+	console.log(names[prop].toLowerCase().charAt(0));
+}
+
